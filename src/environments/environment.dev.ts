6a0dc8c5de5b785ec8bf28a85db4bfd270354f83
Jays4327 <88400448+jschlicker@users.noreply.github.com>
2022-07-05 20:28:43 +0200

    feat(ci): add firebase and tailwind.css

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDMJRMbWcJLVtyATNFz8V-RLDw8LezYzss",
+    authDomain: "athena-4f232.firebaseapp.com",
+    projectId: "athena-4f232",
+    storageBucket: "athena-4f232.appspot.com",
+    messagingSenderId: "113823567934",
+    appId: "1:113823567934:web:742d2cc6019aef02ecc16f",
+    measurementId: "G-6N0R2YBLJH",
+  },
+  production: false,
+};
