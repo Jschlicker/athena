bc948e722130f6f9e48b39b3a10caffcf7558f10
julia <julianschlicker@t-online.de>
2022-07-16 13:47:36 +0200

    Revert "feat: import primeng materials"
    This reverts commit 0b9164b57cbb753c3b4adebdb27d8389a5b7bedc.

--- apps/tale-factory-app/src/app/app.component.ts
+++ apps/tale-factory-app/src/app/app.component.ts
@@ -1,14 +1,10 @@
+import { Component } from '@angular/core';
+
 @Component({
-    selector: "tale-factory-monorepo-root",
-    templateUrl: "./app.component.html",
-    styleUrls: ["./app.component.scss"],
+  selector: 'tale-factory-monorepo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    title = "tale-factory-app";
-
-    constructor(private primengConfig: PrimeNGConfig) {}
-
-    ngOnInit() {
-        this.primengConfig.ripple = true;
-    }
+export class AppComponent {
+  title = 'tale-factory-app';
 }
