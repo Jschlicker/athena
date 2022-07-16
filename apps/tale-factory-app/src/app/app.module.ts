e0360f4da7936a05af2e919f9dc8c28c52504f89
julia <julianschlicker@t-online.de>
2022-07-16 13:38:11 +0200

    Revert "refactor(app): remove nx start-up component"
    This reverts commit eeacc7f58f5f343479d9c08a583b7f7771f9b3c0.

--- apps/tale-factory-app/src/app/app.module.ts
+++ apps/tale-factory-app/src/app/app.module.ts
@@ -1,9 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
-    providers: [],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
