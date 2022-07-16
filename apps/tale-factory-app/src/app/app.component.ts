9e815ccb5562f92a102848f1f0760fd212653815
julia <julianschlicker@t-online.de>
2022-07-16 14:13:00 +0200

    feat(app): integrate primeng to tale-factory-app

--- apps/tale-factory-app/src/app/app.component.ts
+++ apps/tale-factory-app/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PrimeNGConfig } from "primeng/api";
 
 @Component({
-  selector: 'tale-factory-monorepo-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: "tale-factory-monorepo-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'tale-factory-app';
+export class AppComponent implements OnInit {
+    constructor(private primeNgCofnig: PrimeNGConfig) {}
+    title = "tale-factory-app";
+
+    ngOnInit() {
+        this.primeNgCofnig.ripple = true;
+    }
 }
